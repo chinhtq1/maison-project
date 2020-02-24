a2df6b297e7f4f23669b5d6093acbdd792fdbdf1
Taquangchinh <taquangchinh265@gmail.com>
2020-02-24 07:29:44 +0700

    add setting store

---
+++ database/migrations/2020_02_16_085659_create_slides_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSlidesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('slides', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->dateTime('created_at');
+            $table->dateTime('updated_at');
+            $table->string('title');
+            $table->string('slug');
+            $table->boolean('is_public')->default(false);
+            $table->dateTime('date_public')->nullable();
+            $table->text('description')->nullable();
+            $table->json('seo')->nullable();
+            $table->string('fb_link')->default('#')->nullable();
+            $table->json('data')->nullable();
+            $table->integer('user_id')->unsigned()->nullable();
+            $table->foreign('user_id')
+              ->references('id')->on('users')
+              ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('slides');
+    }
+}
